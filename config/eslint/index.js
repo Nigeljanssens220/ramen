952cea368b8663b2de8ebcdd16abb6f409fa9ab7
nigeljanssens <janssensnigel@gmail.com>
2023-05-19 12:58:48 +0200

    chore: add prettier config to all configs and packages

--- config/eslint/index.js
+++ config/eslint/index.js
@@ -1,14 +1,14 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "turbo/no-undeclared-env-vars": "warn",
-    "react/display-name": "warn",
+    '@next/next/no-html-link-for-pages': 'off',
+    'turbo/no-undeclared-env-vars': 'warn',
+    'react/display-name': 'warn',
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
+      presets: [require.resolve('next/babel')],
     },
   },
-};
+}
