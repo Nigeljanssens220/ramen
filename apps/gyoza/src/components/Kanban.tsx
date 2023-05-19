5c125a3fd9363998b75fd942e56cb68bf92135b9
nigeljanssens <janssensnigel@gmail.com>
2023-05-19 16:58:28 +0200

    chore: fix kanban section styling

--- apps/gyoza/src/components/Kanban.tsx
+++ apps/gyoza/src/components/Kanban.tsx
@@ -7,9 +7,9 @@ export const Kanban: React.FC = () => {
   const { data: kanbanColumns, isLoading } = api.column.getAll.useQuery()
 
   return (
-    <div className="flex w-full flex-col items-center gap-y-2 overflow-x-auto p-4">
+    <div className="flex w-full flex-col items-center gap-y-2 overflow-auto p-4">
       <CreateColumnModal />
-      <div className={'flex h-[75vh] items-center justify-center gap-x-2 overflow-x-auto sm:h-[80vh]'}>
+      <div className={'flex h-[75vh] items-center justify-center gap-x-2 sm:h-[80vh]'}>
         {isLoading ? (
           <Ring size={40} lineWeight={5} speed={2} color="#f8fedc" />
         ) : (
